d81abe9f6ba9b7422caea789e24f1c56a91d285d
xiehui <30897863@qq.com>
2018-05-02 22:36:08 +0800

    最简版本

--- test/a.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-const b=require('./b');
-
-b()
--- test/b.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const includes = require('array-includes');
-
-includes.shim();
-function server(a,b){
-    const $this =server.prototype.constructor;
-    console.log($this)
-}
-module.exports= server;
